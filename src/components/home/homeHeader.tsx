b2f7bb4d6ecb47d1ca9dd2a8d6b4447a922e36bc
HezerSantos <HezerSantos24@gmail.com>
2025-08-04 16:39:27 -0500

    Added header and responsive

--- src/components/home/homeHeader.tsx
+++ src/components/home/homeHeader.tsx
@@ -1,6 +1,20 @@
+import { Link } from "react-router-dom"
+
 const HomeHeader: React.FC = () => {
     return(
         <>
+        <header className="page-section home-header-wrapper">
+            <div className="page-section__child home-header">
+                <div className="home-header__info">
+                    <h1>Manage your money with clarity and control</h1>
+                    <p>Track, plan, and spend smarter — with confidence.</p>
+
+                    <Link to={""}>
+                        Get Started
+                    </Link>
+                </div>
+            </div>
+        </header>
         </>
     )
 }

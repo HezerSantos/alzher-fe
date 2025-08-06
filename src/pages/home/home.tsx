6ae84361c3ca9e1a88ee28b065514376ad629eed
HezerSantos <HezerSantos24@gmail.com>
2025-08-06 01:56:44 -0500

    Added the home overview

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,12 +2,14 @@ import HomeHeader from "../../components/home/homeHeader"
 import '../../assets/styles/home/home.css'
 import IndexNav from "../../components/universal/navbar/indexNav"
 import HomeFeatures from "../../components/home/homeFeatures"
+import HomeOverview from "../../components/home/homeOverview"
 const Home: React.FC = () => {
     return(
         <>
             <IndexNav />
             <HomeHeader />
             <HomeFeatures />
+            <HomeOverview />
         </>
     )
 }

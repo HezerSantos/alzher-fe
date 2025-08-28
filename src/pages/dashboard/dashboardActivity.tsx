958f34281443f86508163efd06c73abdefc3f68d
HezerSantos <HezerSantos24@gmail.com>
2025-08-28 00:30:30 -0500

    Progress on activity section of dashboard. completed the search container and skeleton for transaction item

--- src/pages/dashboard/dashboardActivity.tsx
+++ src/pages/dashboard/dashboardActivity.tsx
@@ -3,6 +3,7 @@ import '../../assets/styles/dashboard/dashboard.css'
 import DashboardNav from '../../components/universal/navbar/dashboardNav'
 import DashboardContext from '../../context/dashboard/dashboardContext'
 import { IoIosArrowDropdown } from "react-icons/io";
+import DashboardMiniNav from '../../components/universal/navbar/dashboardMiniNav';
 
 interface FilterButtonProps {
     name: string
@@ -25,7 +26,95 @@ type ToggleFilterType =  (
 
 const toggleFilter: ToggleFilterType = (e, setFilterToggle) => {
     e.currentTarget.classList.toggle("filterButtonToggled")
+    e.currentTarget.parentElement?.classList.toggle("activity-filters-border-style-fix")
     setFilterToggle(prev => !prev)
+}
+
+const TransactionContainerHeader: React.FC = () => {
+    return(
+        <>
+            <div className='transaction-container__header transaction-item'>
+                <input type='checkbox'></input>
+                <div className='transaction-hidden-detail'>
+                    Transaction ID:
+                </div>
+                <div>
+                    Category:
+                </div>
+                <div className='transaction-hidden-detail'>
+                    Description:
+                </div>
+                <div>
+                    Date:
+                </div>
+                <div>
+                    Amount:
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+interface TransactionItemProps {
+    transactionId: string,
+    category: string,
+    description: string,
+    transactionDate: string,
+    transactionAmount: number
+}
+
+const TransactionItem: React.FC<TransactionItemProps> = ({transactionId, category, description, transactionDate, transactionAmount}) => {
+    return(
+        <>
+            <div className='transaction-item'>
+                <input type='checkbox' id={transactionId}></input>
+                <div className='transaction-hidden-detail'>
+                    {transactionId}
+                </div>
+                <div>
+                    {category}
+                </div>
+                <div className='transaction-hidden-detail'>
+                    <p className='transaction-description'>{description}</p>
+                </div>
+                <div>
+                    {transactionDate}
+                </div>
+                <div>
+                    {transactionAmount}
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+const TransactionContainer: React.FC = () => {
+    const test = []
+
+    for(let i = 0; i < 100; i++){
+        test.push(i)
+    }
+    return(
+        <>
+            <form className='transaction-container'>
+                <TransactionContainerHeader />
+                {test.map(item => {
+                    return(
+                        <TransactionItem 
+                            key={item}
+                            transactionId='DO0001'
+                            category='Dining'
+                            description='McDonalds Quarter Cheese Pounder'
+                            transactionDate='07/12/25'
+                            transactionAmount={14.52}
+                        />
+                    )
+                })}
+            </form>
+        </>
+    )
 }
 const DashboardActivity: React.FC = () => {
     const dashboardContext = useContext(DashboardContext)
@@ -37,6 +126,7 @@ const DashboardActivity: React.FC = () => {
             <div className="page-section">
                 <div className={`page-section__child dashboard-parent ${dashboardContext?.isHidden? "d-nav__grid-reset" : ""}`}>
                     <DashboardNav />
+                    <DashboardMiniNav />
                     <div className='dashboard__activity-container'>
                         <div className='activity-header'>
                             <h1>
@@ -70,8 +160,8 @@ const DashboardActivity: React.FC = () => {
                                     </div>
                                 )}
                             </div>
-                        
                         </div>
+                        <TransactionContainer />
                     </div>
                 </div>
             </div>

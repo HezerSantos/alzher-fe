6e2907c8c67360b61d44fb04808d1d7ee604d420
HezerSantos <HezerSantos24@gmail.com>
2025-09-01 01:06:09 -0500

    starte progress on scan page

--- src/pages/dashboard/dashboardScan.tsx
+++ src/pages/dashboard/dashboardScan.tsx
@@ -1,11 +1,64 @@
+import React, { useContext } from 'react'
 import '../../assets/styles/dashboard/dashboard.css'
+import DashboardMiniNav from '../../components/universal/navbar/dashboardMiniNav'
 import DashboardNav from '../../components/universal/navbar/dashboardNav'
+import DashboardContext from '../../context/dashboard/dashboardContext'
+
+const ScanHeader: React.FC = () => {
+    return(
+        <div className='scan-header'>
+            <h1>
+                Scan
+            </h1>
+        </div>
+    )
+}
+
+const ScanForm: React.FC = () => {
+    const handleDragOver = (e) => {
+        e.preventDefault()
+    }
+
+    const handleDrop = (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+        console.log(e.dataTransfer.files)
+    }
+
+    const check = (e) => {
+        e.preventDefault();
+
+        const form = e.target;
+        const input = form.transactionDocuments;
+        console.log(input.files);  // This will log the FileList object
+    }
+    return(
+        <form action="" className='scan-form' onSubmit={(e) => check(e)}>
+            <label htmlFor="file-input"
+                onDrop={(e) => handleDrop(e)}
+                onDragOver={(e) => handleDragOver(e)}
+            
+            >hello</label>
+            <input type="file" id='file-input' name='transactionDocuments'/>
+            <button>
+                submit
+            </button>
+        </form>
+    )
+}
+
 const DashboardScan: React.FC = () => {
+    const dashboardContext = useContext(DashboardContext)
     return(
         <>
             <div className="page-section">
-                <div className='page-section__child dashboard-parent'>
+                <div className={`page-section__child dashboard-parent ${dashboardContext?.isHidden? "d-nav__grid-reset" : ""}`}>
                     <DashboardNav />
+                    <DashboardMiniNav />
+                    <div className='scan-container'>
+                        <ScanHeader />
+                        <ScanForm />
+                    </div>
                 </div>
             </div>
         </>

16c781d3f908ad66b026f59125e8869215fae0a7
HezerSantos <HezerSantos24@gmail.com>
2025-11-04 02:23:46 -0600

    Added server id

--- src/app.config.ts
+++ src/app.config.ts
@@ -2,7 +2,10 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: import.meta.env.MODE === 'production'? import.meta.env.VITE_API_URL : "http://localhost:8080",
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        ['Server-Id']: "HV003"
+    }
 })
 
 export default api
